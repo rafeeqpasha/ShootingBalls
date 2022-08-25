23a48a214347f48822f4a8e5fadfc9b792ffcc7c
Rafeeq Pasha <rafeeqpasha88@gmail.com>
2022-08-25 13:38:20 +0530

    add shootingball

---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject arCamera;
+  //  public GameObject smaoke;
+
+    public void Shooting()
+    {
+        RaycastHit hit;
+        if( Physics.Raycast(arCamera.transform.position, arCamera.transform.forward, out hit))
+        {
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
